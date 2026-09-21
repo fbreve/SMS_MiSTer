952f962cdb7af1a3889a6308793ce49b2508f9d4
Fabricio Breve <fbreve@gmail.com>
2026-09-21 15:35:26 -0300

    Compile Evolution mapper in T80 trace runner

--- sim/evolution/run_t80_trace.sh
+++ sim/evolution/run_t80_trace.sh
@@ -6,6 +6,7 @@ cd "$ROOT"
 for f in rtl/T80/T80_ALU.vhd rtl/T80/T80_MCode.vhd rtl/T80/T80_Reg.vhd rtl/T80/T80.vhd rtl/T80/T80s.vhd; do
   "$GHDL" -a --std=08 "$f"
 done
+"$GHDL" -a --std=08 rtl/evolution_mapper.vhd
 "$GHDL" -a --std=08 sim/evolution/evolution_t80_bios_trace_tb.vhd
 "$GHDL" -e --std=08 evolution_t80_bios_trace_tb
 "$GHDL" -r --std=08 evolution_t80_bios_trace_tb \
